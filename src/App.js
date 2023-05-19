0a12397f1779d825678a2e56c38d2fb91c7b97e9
axalbeda <57092780+axalbeda@users.noreply.github.com>
2023-05-19 12:07:18 -0400

    hashHomepa6gess

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Header1 />
-      <HashRouter>
+      <HashRouter basename={process.env.HomePage}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/createbook" element={<CreateBook />} />
